6606efbeb68dfe52268bf35302e97fcd92d1fff5
zanydruid <zanydruid528@gmail.com>
2017-07-26 19:59:59 -0400

    Finished DBOutputWritable.

--- src/main/java/ngram/DBOutputWritable.java
+++ src/main/java/ngram/DBOutputWritable.java
@@ -7,14 +7,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBOutputWritable implements DBWritable {
+    // we store n-gram to predict 1-gram
+    private String starting_phrase;
+    private String following_word;
+    private int count;
+
+    public DBOutputWritable(String starting_phrase, String following_word, int count) {
+        this.starting_phrase = starting_phrase;
+        this.following_word = following_word;
+        this.count = count;
+    }
 
     @Override
     public void write(PreparedStatement statement) throws SQLException {
-
+        statement.setString(1, this.starting_phrase);
+        statement.setString(2,this.following_word);
+        statement.setInt(3,this.count);
     }
 
     @Override
     public void readFields(ResultSet resultSet) throws SQLException {
-
+        this.starting_phrase = resultSet.getString(1);
+        this.following_word = resultSet.getString(2);
+        this.count = resultSet.getInt(3);
     }
 }
